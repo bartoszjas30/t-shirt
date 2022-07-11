752f24003cd9ca39a442f18a495baa749e53844f
bartoszjas30 <bartoszjas20@gmail.com>
2022-07-11 15:33:52 +0200

    everything is done

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import styles from './Button.module.scss';
+import clsx from 'clsx';
+
+const Button = ({ children, className} ) => {
+    return (<button className={clsx(styles.button, className)}>{children}</button>);
+};
+
+export default Button;
+
+//
